593a0368304c11affd55ac74e63b40b652ca13ec
tienhoai68 <nguyentien1668@gmail.com>
2023-08-20 19:12:50 +0700

    update chair

--- src/components/ListChair.jsx
+++ src/components/ListChair.jsx
@@ -3,11 +3,18 @@ import React, { Component } from "react";
 export default class ListChair extends Component {
   render() {
     const chair = this.props.element.danhSachGhe;
-      console.log(chair[this.props.index].soGhe);
+      // console.log(chair[this.props.index].soGhe);
+      console.log(this.props.element);
     return (
         <tr>
           <td>{this.props.element.hang}</td>
-          <td><button>{chair[0].soGhe}</button></td>
+          {
+            this.props.element.danhSachGhe.map((item)=>{
+              return  <td><button>{item.soGhe}</button></td>
+            })
+          }
+         
+          {/* <td><button>{chair[0].soGhe}</button></td>
           <td><button>{chair[1].soGhe}</button></td>
           <td><button>{chair[2].soGhe}</button></td>
           <td><button>{chair[3].soGhe}</button></td>
@@ -18,7 +25,7 @@ export default class ListChair extends Component {
           <td><button>{chair[8].soGhe}</button></td>
           <td><button>{chair[9].soGhe}</button></td>
           <td><button>{chair[10].soGhe}</button></td>
-          <td><button>{chair[11].soGhe}</button></td>
+          <td><button>{chair[11].soGhe}</button></td> */}
         </tr>
     );
   }
